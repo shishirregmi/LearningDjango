ef5700638a647c962af31da577f97323aa62363b
Shishir Regmi <rshishir21@gmail.com>
2022-07-20 07:59:17 +0545

    CRUD for agent completed

--- agent/urls.py
+++ agent/urls.py
@@ -2,7 +2,9 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    #path("<int:id>",views.index,name="index"),
+    path("<int:id>/",views.agentDetail,name="agent-detail"),
+    path("<int:id>/delete",views.agentDelete,name="agent-delete"),
+    path("<int:id>/edit/",views.updateAgent,name="agent-update"),
     path("create/",views.addAgent,name="create-agent"),
     path("",views.index,name="home-agent"),
 ]
--- agent/views.py
+++ agent/views.py
@@ -1,6 +1,7 @@
 import logging
 from django.http import HttpResponseRedirect
-from django.shortcuts import redirect, render
+from django.shortcuts import get_object_or_404, redirect, render
+from django.contrib import messages
 
 from agent.forms import CreateNewAgent
 from agent.models import Agent
@@ -24,7 +25,27 @@ def index(request):
     data = Agent.objects.all
     return render(request,'agent/index.html',{'datas':data})
 
-def updateAgent(UpdateView):
-    form_class  = CreateNewAgent
-    redirect_field_name = 'agent/create.html'
-    model = Agent
+def agentDetail(request, id):
+    data = Agent.objects.get(id=id)
+    return render(request,'agent/details.html',{'data':data})
+
+def agentDelete(request, id):
+    obj = get_object_or_404(Agent, id = id)  
+    if request.method =="POST":
+        obj.delete()
+        return redirect('home-agent') 
+    return render(request, "delete_view.html")
+
+def updateAgent(request,id):
+    data = get_object_or_404(Agent,id=id)
+    form = CreateNewAgent(request.POST or None, instance= data)
+    context= {'form': form}
+    if form.is_valid():
+        obj= form.save(commit= False)
+        obj.save()
+        messages.success(request, "You successfully updated the post")
+        context= {'form': form}
+        return render(request, 'agent/update.html', context)
+    else:
+        context= {'form': form,'error': 'The form was not updated successfully. Please enter in a title and content'}
+        return render(request,'agent/update.html' , context)
